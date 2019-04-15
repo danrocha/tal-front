f90c1af347c1bc377981c7ea3a0980c4a50d10ae
Daniel da Rocha <danrocha@gmail.com>
2019-04-15 06:41:48 +0200

    fix: extract logout action to component and slot scopes

---
+++ src/components/Logout.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+
+    <slot :logout="logout"/>
+
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { onLogout } from '../vue-apollo';
+
+  export default {
+    name: 'Logout',
+    methods: {
+    async logout() {
+      await firebase.auth().signOut();
+      await onLogout(this.$apollo.provider.defaultClient);
+      this.$router.push('/');
+    },
+  },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
