1e9bffed9559f492058220ae96042256f8d9f709
Daniel da Rocha <danrocha@gmail.com>
2019-04-15 06:36:17 +0200

    fix: fix auth formatting

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -2,12 +2,12 @@
   <div class="pt-8 container mx-auto md:w-auto md:mr-6 md:ml-56">
     <vue-headful title="Login or Sign Up - TAL"/>
     <h2>Login or Sign Up</h2>
-    <div class="flex">
+    <div>
       <p class="w-1/2">
         While you can use most of the site without registering, you need an account to add office,
         vote on office, and to edit some of the detailed office information.
       </p>
-      <div class="w-64 py-4 ml-8 border border-gray-500 rounded" id="firebaseui-auth-container"></div>
+      <div class="w-1/3 py-4 border border-gray-300" id="firebaseui-auth-container"></div>
     </div>
   </div>
 </template>
