ce300a300f55ec617fa521c15f25a620de66acf9
Daniel da Rocha <danrocha@gmail.com>
2019-04-17 08:01:02 +0200

    fix: styling

--- src/components/TosPrivacyLinks.vue
+++ src/components/TosPrivacyLinks.vue
@@ -1,6 +1,6 @@
 <template>
-  <p class="text-sm text-center">
-    We respect your privacy. <br />Please read our
+  <p class="text-sm text-gray-700">
+    We respect your privacy. Please read our
     <router-link to="tos" class="link">Terms of Service</router-link>&nbsp;and
     <router-link to="privacy" class="link">Privacy Policy</router-link>.
   </p>
