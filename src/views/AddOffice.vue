9b5ba588f1cf83875d15b337a998a1d10b4fc75b
Daniel da Rocha <danrocha@gmail.com>
2019-04-25 07:45:32 +0200

    fix(add office): go to office page after adding

--- src/views/AddOffice.vue
+++ src/views/AddOffice.vue
@@ -2,9 +2,9 @@
   <div class="pt-8 container mx-auto md:w-2/3 lg:w-1/2 md:mr-6 md:ml-56">
     <h2>Add Office</h2>
     <div class="mt-8">
-      <add-office-1-city />
-      <add-office-2-details />
-      <add-office-3-review @save="addNewOffice" :is-loading="isLoading" />
+      <add-office-1-city/>
+      <add-office-2-details/>
+      <add-office-3-review @save="addNewOffice" :is-loading="isLoading"/>
     </div>
   </div>
 </template>
@@ -13,6 +13,7 @@
 import AddOffice3Review from '@/components/AddOffice/AddOffice3Review.vue';
 import AddOffice2Details from '@/components/AddOffice/AddOffice2Details.vue';
 import AddOffice1City from '@/components/AddOffice/AddOffice1City.vue';
+import kebabCase from '@/mixins/kebabCase';
 import { mapState, mapActions } from 'vuex';
 const ADD_OFFICE_MUTATION = require('@/graphql/AddOfficeMutation.gql');
 const LOCATIONS = require('@/graphql/Locations.gql');
@@ -20,6 +21,7 @@ import CITIES from '@/graphql/Cities.gql';
 
 export default {
   name: 'AddOffice',
+  mixins: [kebabCase],
   components: {
     AddOffice1City,
     AddOffice2Details,
@@ -28,6 +30,7 @@ export default {
   data() {
     return {
       isLoading: false,
+      locations: null,
     };
   },
   beforeMount() {
@@ -47,24 +50,36 @@ export default {
   computed: {
     mutationVariables() {
       if (this.addOffice.officeDetails) {
-        let country = this.addOffice.officeDetails.address_components.find(component => {
-          return component.types.includes('country');
-        });
-        let city = this.addOffice.officeDetails.address_components.find(component => {
-          return component.types.includes('locality');
-        });
-        let region = this.addOffice.officeDetails.address_components.find(component => {
-          return component.types.includes('administrative_area_level_1');
-        });
-        let streetNumber = this.addOffice.officeDetails.address_components.find(component => {
-          return component.types.includes('street_number');
-        });
-        let route = this.addOffice.officeDetails.address_components.find(component => {
-          return component.types.includes('route');
-        });
-        let postalCode = this.addOffice.officeDetails.address_components.find(component => {
-          return component.types.includes('postal_code');
-        });
+        let country = this.addOffice.officeDetails.address_components.find(
+          component => {
+            return component.types.includes('country');
+          }
+        );
+        let city = this.addOffice.officeDetails.address_components.find(
+          component => {
+            return component.types.includes('locality');
+          }
+        );
+        let region = this.addOffice.officeDetails.address_components.find(
+          component => {
+            return component.types.includes('administrative_area_level_1');
+          }
+        );
+        let streetNumber = this.addOffice.officeDetails.address_components.find(
+          component => {
+            return component.types.includes('street_number');
+          }
+        );
+        let route = this.addOffice.officeDetails.address_components.find(
+          component => {
+            return component.types.includes('route');
+          }
+        );
+        let postalCode = this.addOffice.officeDetails.address_components.find(
+          component => {
+            return component.types.includes('postal_code');
+          }
+        );
         return {
           name: this.addOffice.officeDetails.name,
           website: this.addOffice.officeDetails.website,
@@ -81,8 +96,11 @@ export default {
           lat: this.addOffice.officeDetails.geometry.location.lat(),
           lng: this.addOffice.officeDetails.geometry.location.lng(),
           formattedAddress: this.addOffice.officeDetails.formatted_address,
-          addressComponents: JSON.stringify(this.addOffice.officeDetails.address_components),
-          internationalPhoneNumber: this.addOffice.officeDetails.international_phone_number,
+          addressComponents: JSON.stringify(
+            this.addOffice.officeDetails.address_components
+          ),
+          internationalPhoneNumber: this.addOffice.officeDetails
+            .international_phone_number,
           googlePlaceId: this.addOffice.officeDetails.place_id,
         };
       }
@@ -91,57 +109,26 @@ export default {
     ...mapState({
       addOffice: state => state.addOffice,
       user: state => state.user.user,
+      selectedLocation: state => state.location.selectedLocation,
     }),
   },
   methods: {
+    ...mapActions([
+      'resetAddOffice',
+      'resetAddOfficeOfficeDetails',
+      'setItemsPerPage',
+      'location/setLocation',
+    ]),
     addNewOffice() {
       this.isLoading = true;
-
       // We save the user input in case of an error
       const newOffice = this.mutationVariables;
-      // We clear it early to give the UI a snappy feel
-      //this.newTag = '';
-      const locationVariables = {
-        after: null,
-        before: null,
-
-        first: this.$store.state.itemsPerPage,
-        last: null,
-
-        orderBy: 'CREATED_AT_DESC',
-        condition: {},
-        filter: { id: { notIn: [] } },
-      };
-
-      // Call to the graphql mutation
       this.$apollo
         .mutate({
           // Query
           mutation: ADD_OFFICE_MUTATION,
           // Parameters
           variables: newOffice,
-
-          // Update the cache with the result
-          // The query will be updated with the optimistic response
-          // and then with the real result of the mutation
-          update: (store, { data: { addOffice } }) => {
-            // Read the data from our cache for this query.
-            const data = store.readQuery({
-              query: LOCATIONS,
-              variables: locationVariables,
-            });
-
-            // Add our tag from the mutation to the end
-            data.locations.nodes.unshift(addOffice.location);
-            this.setLocation(addOffice.location.id);
-
-            // Write our data back to the cache.
-            store.writeQuery({
-              query: LOCATIONS,
-              variables: locationVariables,
-              data,
-            });
-          },
           refetchQueries: [
             {
               query: CITIES,
@@ -153,9 +140,10 @@ export default {
           awaitRefetchQueries: true,
           ignoreResults: false,
         })
-        .then(data => {
-          // Result
+        .then(({ data: { addOffice } }) => {
+          this['location/setLocation'](addOffice.location);
 
+          // Result
           this.$store.dispatch('notification/add', {
             type: 'success',
             message: 'Office location added successfully!',
@@ -169,22 +157,24 @@ export default {
           this.isLoading = false;
           this.$store.dispatch('notification/add', {
             type: 'error',
-            message: 'There was a problem adding this location: ' + error.message,
+            message:
+              'There was a problem adding this location: ' + error.message,
           });
           // We restore the initial user input
           //this.newTag = newTag;
         });
     },
-    ...mapActions([
-      'resetAddOffice',
-      'resetAddOfficeOfficeDetails',
-      'setItemsPerPage',
-      'setLocation',
-    ]),
 
     done() {
       this.resetAddOffice();
-      this.$router.push('/');
+      this.$router.push(this.officeDetailsLink());
+    },
+    officeDetailsLink() {
+      return `/${this.kebabCase(
+        this.selectedLocation.countryByCountryIsocode.iso
+      )}/${this.kebabCase(this.selectedLocation.city.name)}/${
+        this.selectedLocation.id
+      }/${this.kebabCase(this.selectedLocation.office.name)}`;
     },
   },
   watch: {
