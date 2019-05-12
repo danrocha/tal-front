a558ffea57bcd642ea0bec76919e2ff3902b1137
Daniel da Rocha <danrocha@gmail.com>
2019-05-12 12:14:20 +0200

    vue config changes

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,28 @@ module.exports = {
         target: 'localhost:8349',
         changeOrigin: true,
       },
+      '/clearbit_logo': {
+        target: 'https://logo.clearbit.com',
+        changeOrigin: true,
+      },
+      '/clearbit_company': {
+        target: 'https://company.clearbit.com',
+        changeOrigin: true,
+      },
     },
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.transpileOptions = {
+          transforms: {
+            dangerousTaggedTemplateString: true,
+          },
+        };
+        return options;
+      });
   },
 };
