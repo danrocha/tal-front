33331abcc4e84735f499b8743e32d898645438d2
Daniel da Rocha <danrocha@gmail.com>
2019-05-14 07:49:07 +0200

    feat: add spinner to office loading

--- src/components/AdminOffices.vue
+++ src/components/AdminOffices.vue
@@ -1,9 +1,16 @@
 <template>
   <apollo-query :query="require('../graphql/Offices.gql')">
-    <template slot-scope="{ result: { loading, error, data } }">
+    <template slot-scope="{ result: { error, data }, isLoading }">
       <h3>Offices</h3>
       <!-- Loading -->
-      <div v-if="loading" class="loading apollo">Loading...</div>
+      <div v-if="isLoading" class="pt-8">
+        <hollow-dots-spinner
+          :animation-duration="1000"
+          :dot-size="15"
+          :dots-num="3"
+          color="#f2d024"
+        />
+      </div>
 
       <!-- Error -->
       <div v-else-if="error" class="error apollo">An error occurred</div>
@@ -20,11 +27,13 @@
 </template>
 
 <script>
+import { HollowDotsSpinner } from 'epic-spinners';
 import AdminOfficesList from './AdminOfficesList.vue';
 export default {
   name: 'AdminOffices',
   components: {
     AdminOfficesList,
+    HollowDotsSpinner,
   },
 };
 </script>
