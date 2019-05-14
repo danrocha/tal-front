11169ee6837e5195f98944d7fd2320d401b99472
Daniel da Rocha <danrocha@gmail.com>
2019-05-14 07:49:58 +0200

    fix: admin office logo

--- src/components/AdminOfficeLogo.vue
+++ src/components/AdminOfficeLogo.vue
@@ -1,12 +1,14 @@
 <template>
-  <div >
-    <div  v-if="!office.logoUrl || office.logoUrl === 'default'" class="flex items-center ">
-      <office-logo-temporary :name="office.name"/>
-      <p v-if="office.logoUrl === 'default'" class="text-xs ml-2 text-gray-500">using default logo</p>
-      <base-button v-if="office.logoUrl !== 'default' && office.website" @click="fetchLogo" class="ml-2">get logo</base-button>
+  <div>
+    <div v-if="!office.logoUrl" class="flex items-center">
+      <office-logo-temporary :name="office.name" />
+      <p v-if="office.logoUrl === 'default'" class="text-xs ml-2 text-gray-500">
+        using default logo
+      </p>
+      <base-button v-if="office.website" @click="fetchLogo" class="ml-2">get logo</base-button>
     </div>
-    <div v-else class="flex  items-center ">
-      <img :src="office.logoUrl" class="w-24 h-24 rounded-full">
+    <div v-else class="flex items-center">
+      <img :src="office.logoUrl" class="w-24 h-24 rounded" />
       <base-button @click="deleteLogo" class="ml-2">delete logo</base-button>
     </div>
   </div>
@@ -17,11 +19,10 @@ import axios from 'axios';
 import { mapActions } from 'vuex';
 import formatUrl from '../mixins/formatUrl';
 
-
 import OfficeLogoTemporary from '../components/OfficeLogoTemporary.vue';
 export default {
   name: 'AdminOfficeLogo',
-  mixins:[formatUrl],
+  mixins: [formatUrl],
   props: {
     office: {
       type: Object,
@@ -33,8 +34,8 @@ export default {
   },
   data() {
     return {
-      logo: ''
-    }
+      logo: '',
+    };
   },
   methods: {
     ...mapActions(['notification/add']),
@@ -53,39 +54,43 @@ export default {
             type: 'error',
             message: 'No logo found',
           });
-          this.saveLogo('default')
+          //this.saveLogo('default')
         });
     },
     saveLogo(logoUrl) {
-      this.$apollo.mutate({
-        mutation: require('../graphql/UpdateOfficeById.gql'),
-        variables: {
-          input: {
-            id: this.office.id,
-            patch: {
-              logoUrl
-            }
-          }
-        },
-        refetchQueries: [
-          {
-            query: require('../graphql/Offices.gql')
-          }]
-      }).then(() => {
+      this.$apollo
+        .mutate({
+          mutation: require('../graphql/UpdateOfficeById.gql'),
+          variables: {
+            input: {
+              id: this.office.id,
+              patch: {
+                logoUrl,
+              },
+            },
+          },
+          refetchQueries: [
+            {
+              query: require('../graphql/Offices.gql'),
+            },
+          ],
+        })
+        .then(() => {
           this['notification/add']({
             type: 'success',
             message: 'Office Logo updated!',
           });
-      }).catch(()=> {
-        this['notification/add']({
+        })
+        .catch(() => {
+          this['notification/add']({
             type: 'error',
             message: 'There was an error saving to the DB!',
           });
-      })
+        });
     },
     deleteLogo() {
-      console.log('delete')
-    }
+      console.log('delete');
+    },
   },
 };
 </script>
