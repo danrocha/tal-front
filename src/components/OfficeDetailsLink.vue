f86264eff9a7dddf97a702230df23604332f4b54
Daniel da Rocha <danrocha@gmail.com>
2019-05-20 07:27:38 +0200

    fix: set location when clicking on recently added office link

--- src/components/OfficeDetailsLink.vue
+++ src/components/OfficeDetailsLink.vue
@@ -1,11 +1,12 @@
 <template>
-  <router-link :to="officeDetailsLink()" class="link" tag="a">
-    <slot />
+  <router-link :to="officeDetailsLink()" class="link" tag="a" @click.native="setLocation">
+    <slot/>
   </router-link>
 </template>
 
 <script>
 import kebabCase from '../mixins/kebabCase.js';
+import { mapActions } from 'vuex';
 export default {
   mixins: [kebabCase],
   props: {
@@ -15,11 +16,17 @@ export default {
     },
   },
   methods: {
+    ...mapActions(['location/setLocation']),
+    setLocation() {
+      this['location/setLocation'](this.location);
+    },
     officeDetailsLink() {
       return {
-        path: `/${this.kebabCase(this.location.city.countryByCountryIsocode.iso)}/${this.kebabCase(
-          this.location.city.name
-        )}/${this.location.id}/${this.kebabCase(this.location.office.name)}`,
+        path: `/${this.kebabCase(
+          this.location.city.countryByCountryIsocode.iso
+        )}/${this.kebabCase(this.location.city.name)}/${
+          this.location.id
+        }/${this.kebabCase(this.location.office.name)}`,
       };
     },
   },
